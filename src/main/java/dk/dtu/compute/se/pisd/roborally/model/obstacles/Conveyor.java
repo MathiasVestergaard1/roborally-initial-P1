d85028ffe1b3718b760e8f06ab67a884ad2c7ced
Pyrolin <113906792+Pyrolin@users.noreply.github.com>
2023-05-04 13:20:03 +0200

    H er ballin

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/obstacles/Conveyor.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/obstacles/Conveyor.java
@@ -4,12 +4,10 @@ import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
 public class Conveyor extends Obstacle {
-
-
     public Conveyor(Space space, String color, Heading heading) {
         super(space, color, heading);
-
     }
 }
+
 
 
