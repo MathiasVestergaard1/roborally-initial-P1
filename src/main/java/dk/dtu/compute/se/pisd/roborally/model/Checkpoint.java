72b63d850ab2b70b494893718f934675439d8d03
ACE1960 <113360654+ACE1960@users.noreply.github.com>
2023-05-04 15:50:02 +0200

    javadoc to checkpoint

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Checkpoint.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Checkpoint.java
@@ -1,5 +1,13 @@
 package dk.dtu.compute.se.pisd.roborally.model;
 
+/**
+ * This class define "checkpoint" whisch has three instance variables "board", "x" and "y".
+ * "Board" is an instance of the "board" class and it is marked as "final,
+ * "x" and "y" are two integers that represent the coordinates of a point on the board.
+ * The class has a constructor that takes three parameters, "board", "x" and "y",
+ * and initializes the corresponding instance with the given values.
+ * @author Ali Can Erten
+ */
 public class Checkpoint {
 
     public final Board board;
